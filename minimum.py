689858119a35b851688894e36f70f4d5c56f5ac8
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-09-28 15:15:15 +0600

    Add files via upload

---
+++ minimum.py
@@ -0,0 +1,7 @@
+n1= int(input('First number: '))
+n2= int(input('Second number: '))
+n3= int(input('Third number: '))
+
+Lowest = min(n1, n2, n3)
+
+print('Lowest number among three numbers is: ', Lowest)
