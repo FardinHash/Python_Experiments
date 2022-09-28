da89d51ea5ff4c7b444195b4752edfe4b726e7d1
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-09-28 15:20:39 +0600

    Update Average.py

--- Average.py
+++ Average.py
@@ -1,8 +1,9 @@
+#Take input from user
 n1= int(input('First number: '))
 n2= int(input('Second number: '))
 n3= int(input('Third number: '))
 
-total= n1+n2+n3
-Average = total/3
+total= n1+n2+n3  #total of three given numbers
+Average = total/3  #Calculate average
 
-print('Average is: ', Average)
+print('Average is: ', Average)  #print the result
