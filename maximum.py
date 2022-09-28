792430dc0f1f448bb638226a0a15c78c9c53b2b5
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-09-28 15:16:51 +0600

    Update maximum.py

--- maximum.py
+++ maximum.py
@@ -1,7 +1,8 @@
+#take input from user
 n1= int(input('First number: '))
 n2= int(input('Second number: '))
 n3= int(input('Third number: '))
 
-Maximum = max(n1, n2, n3)
+Maximum = max(n1, n2, n3)  #Calculate the highest number
 
-print('Highest number among three numbers is: ', Maximum)
+print('Highest number among three numbers is: ', Maximum)  #Print the number
