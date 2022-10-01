339ca639a6834a5c15451ba916457f91753fcf9d
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-10-02 02:41:45 +0600

    Add files via upload

---
+++ grade-calculator.py
@@ -0,0 +1,19 @@
+print('Enter your marks:')
+cour1= int(input('Course 1: '))
+cour2= int(input('Course 2: '))
+cour3= int(input('Course 3: '))
+cour4= int(input('Course 4: '))
+
+
+result= (cour1 + cour2 + cour3 + cour4)/4
+
+if result>= 90:
+    print('Grade: A')
+elif result>= 80:
+    print('Grade: B')
+elif result>= 70:
+    print('Grade: C')
+elif result>= 60:
+    print('Grade: D')
+else:
+    print('Retake!')
